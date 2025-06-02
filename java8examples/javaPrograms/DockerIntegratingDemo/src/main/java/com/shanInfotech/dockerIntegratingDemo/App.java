47ba01c2c6fd7908c5fcfac0b8b868914691f979
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-06-02 13:13:56 +0530

    Removed accidental recursive java8examples folder

--- java8examples/javaPrograms/DockerIntegratingDemo/src/main/java/com/shanInfotech/dockerIntegratingDemo/App.java
+++
@@ -1,7 +0,0 @@
-package com.shanInfotech.dockerIntegratingDemo;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Ganesha! Hello from Dockerized Maven App!");
-    }
-}
