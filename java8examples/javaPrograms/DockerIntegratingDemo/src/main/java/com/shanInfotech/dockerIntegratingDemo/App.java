1c28a7d19f18f54a75aa01460a698caa5556db81
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-06-02 13:00:06 +0530

    Fresh init for clean Git tracking

---
+++ java8examples/javaPrograms/DockerIntegratingDemo/src/main/java/com/shanInfotech/dockerIntegratingDemo/App.java
@@ -0,0 +1,7 @@
+package com.shanInfotech.dockerIntegratingDemo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Ganesha! Hello from Dockerized Maven App!");
+    }
+}
